4cdc4ac5e4de7f4c213af7080e71a9950ae12953
kimtaehyun-cmd <xogus_04@naver.com>
2024-09-27 17:57:23 +0900

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const express = require('express'); // express 모듈 불러오기
+const express = require('express'); // express 모듈 불러오기
 const cors = require('cors'); // cors 모듈 불러오기
-// const PORT = '8080';
+const PORT = '8080';
 
-// const app = express(); // express 모듈을 사용하기 위해 app 변수에 할당한다.
+const app = express(); // express 모듈을 사용하기 위해 app 변수에 할당한다.
 
 // const corsOptions = {
 //   origin: 'http://localhost:3000', // 허용할 주소
@@ -32,47 +32,47 @@ app.use(require('./routes/postRoutes'));
 app.use(require('./routes/deleteRoutes'));
 app.use(require('./routes/updateRoutes'));
 
-// app.listen(PORT, () => console.log(`Server is running on ${PORT}`)); // 서버 실행 시 메시지
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`)); // 서버 실행 시 메시지
 
 // ----------------------------------------------------------------------------
-const express = require('express');
-const { spawn } = require('child_process');
-const path = require('path');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const { spawn } = require('child_process');
+// const path = require('path');
+// const bodyParser = require('body-parser');
 
-// console.log(path.join(__dirname)); 루트 경로 확인
+// // console.log(path.join(__dirname)); 루트 경로 확인
 
-const app = express();
-const port = 8000;
-app.use(bodyParser.json());
+// const app = express();
+// const port = 8000;
+// app.use(bodyParser.json());
 
-app.post('/chat', (req, res) => {
-  const sendQuestion = req.body.question;
-  const execPython = path.join(__dirname, 'chat', 'bizchat.py');
-  const pythonPath = path.join(__dirname, 'venv', 'bin', 'python3');
+// app.post('/chat', (req, res) => {
+//   const sendQuestion = req.body.question;
+//   const execPython = path.join(__dirname, 'chat', 'bizchat.py');
+//   const pythonPath = path.join(__dirname, 'venv', 'bin', 'python3');
 
-  const net = spawn(pythonPath, [execPython, sendQuestion]);
+//   const net = spawn(pythonPath, [execPython, sendQuestion]);
 
-  output = '';
+//   output = '';
 
-  //파이썬 파일 수행 결과를 받아온다
-  net.stdout.on('data', function (data) {
-    output += data.toString();
-  });
+//   //파이썬 파일 수행 결과를 받아온다
+//   net.stdout.on('data', function (data) {
+//     output += data.toString();
+//   });
 
-  net.on('close', (code) => {
-    if (code === 0) {
-      res.status(200).json({ answer: output });
-    } else {
-      res.status(500).send('Something went wrong');
-    }
-  });
+//   net.on('close', (code) => {
+//     if (code === 0) {
+//       res.status(200).json({ answer: output });
+//     } else {
+//       res.status(500).send('Something went wrong');
+//     }
+//   });
 
-  net.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-});
+//   net.stderr.on('data', (data) => {
+//     console.error(`stderr: ${data}`);
+//   });
+// });
 
-app.listen(port, () => {
-  console.log('Server is running on port 8000');
-});
+// app.listen(port, () => {
+//   console.log('Server is running on port 8000');
+// });
